9adc9f612d27f42cbaf2e9f37a5cb397cb7f5397
Mixa1997 <43315299+Mixa1997@users.noreply.github.com>
2020-10-16 13:12:42 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Средний
+            try
+            {
+                double U, k, y;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Введите k:");
+                k = double.Parse(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write("Введите y:");
+                y = double.Parse(Console.ReadLine());
+                U = (Math.Log(2,k + 4.3)) / (Math.Exp(k + y) + Math.Sqrt(y));
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("Результат U:" + U);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
